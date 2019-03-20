cb0c6941a3d572ac2e68d1a36196a873695db1aa
Piyush Sharma <piyush_sharma2@apple.com>
2019-03-20 12:01:36 -0700

    first commit

---
+++ CollectionViewDynamic/ViewController.swift
@@ -0,0 +1,145 @@
+
+import UIKit
+
+class Cell: UICollectionViewCell {
+    
+    let label = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        label.numberOfLines = 0
+        backgroundColor = .orange
+        contentView.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor).isActive = true
+        label.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor).isActive = true
+        label.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        layoutIfNeeded()
+        let layoutAttributes = super.preferredLayoutAttributesFitting(layoutAttributes)
+        layoutAttributes.bounds.size = systemLayoutSizeFitting(UIView.layoutFittingCompressedSize, withHorizontalFittingPriority: .required, verticalFittingPriority: .defaultLow)
+        return layoutAttributes
+    }
+    
+}
+
+class FlowLayout: UICollectionViewFlowLayout {
+    
+    override init() {
+        super.init()
+        
+        self.minimumInteritemSpacing = 10
+        self.minimumLineSpacing = 10
+        self.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutAttributesForItem(at indexPath: IndexPath) -> UICollectionViewLayoutAttributes? {
+        guard let layoutAttributes = super.layoutAttributesForItem(at: indexPath) else { return nil }
+        guard let collectionView = collectionView else { return nil }
+        layoutAttributes.bounds.size.width = collectionView.safeAreaLayoutGuide.layoutFrame.width - sectionInset.left - sectionInset.right
+        return layoutAttributes
+    }
+    
+    override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let superLayoutAttributes = super.layoutAttributesForElements(in: rect) else { return nil }
+        guard scrollDirection == .vertical else { return superLayoutAttributes }
+        
+        let computedAttributes = superLayoutAttributes.compactMap { layoutAttribute in
+            return layoutAttribute.representedElementCategory == .cell ? layoutAttributesForItem(at: layoutAttribute.indexPath) : layoutAttribute
+        }
+        return computedAttributes
+    }
+    
+    override func shouldInvalidateLayout(forBoundsChange newBounds: CGRect) -> Bool {
+        return true
+    }
+    
+}
+
+
+class CollectionViewController: UICollectionViewController {
+    
+    var items = [
+        "Lorem ipsum.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
+        "Lorem ipsum dolor sit amet.",
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+    ]
+    
+    let columnLayout = FlowLayout()
+    let textField = UITextField()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        textField.delegate = self
+        collectionView?.contentInset.bottom = 70
+        collectionView.alwaysBounceVertical = true
+        collectionView.collectionViewLayout = columnLayout
+        collectionView.contentInsetAdjustmentBehavior = .always
+        collectionView.register(Cell.self, forCellWithReuseIdentifier: "Cell")
+        configureKeyboardDelegates()
+        textField.backgroundColor = .red
+        textField.frame = CGRect(x: 0, y: view.bounds.height-100, width: view.bounds.width, height: 60)
+        view.addSubview(textField)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! Cell
+        cell.label.text = items[indexPath.row]
+        return cell
+    }
+}
+
+extension CollectionViewController {
+    private func configureKeyboardDelegates() {
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboard), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleKeyboard), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func handleKeyboard(notification: Notification) {
+        if let userInfo = notification.userInfo {
+            if let keyboardSize = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                if notification.name == UIResponder.keyboardWillShowNotification {
+                    textField.frame.origin.y = view.bounds.height-keyboardSize.height-60
+                }
+            }
+        }
+    }
+}
+
+extension CollectionViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        self.collectionView?.performBatchUpdates({
+            let indexPath = IndexPath(item: self.items.count, section: 0)
+            items.append(textField.text!)
+            self.collectionView?.insertItems(at: [indexPath])
+            DispatchQueue.main.async {
+                self.collectionView?.scrollToItem(at: indexPath, at: .bottom, animated: true)
+            }
+        }, completion: nil)
+        
+       return true
+        
+    }
+}
